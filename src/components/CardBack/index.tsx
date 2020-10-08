ccec000d9fcb175413cbc025bbef6e1a82e480fb
Stardust <stardustdev@outlook.com>
2020-10-08 08:25:15 -0400

    Add redux, react-redux, redux-thunk dependency

---
+++ src/components/CardBack/index.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+interface IProps {
+  width: number;
+}
+
+const CardBack: React.FC<IProps> = ({ width }: IProps): JSX.Element => {
+  return <></>;
+};
+
+export default memo(CardBack);
