bafaf5f02a9dbdefce09211eea0820c83582d44e
Stardust <stardustdev@outlook.com>
2020-10-08 14:14:10 -0400

    update file structure

--- src/view/preview/index.tsx
+++ src/view/preview/index.tsx
@@ -6,36 +6,39 @@ import Card from 'src/models/Card';
 import CardBackImage from 'src/assets/images/card-back.jpg';
 import { AppState } from 'src/redux/reducer';
 import { suffleCardsCreator, dealCardsCreator } from 'src/redux/card/actions';
+import './styles.css';
 
 const Preview: React.FC<{}> = (): JSX.Element => {
   const dispatch = useDispatch();
+  const [isDealed, setIsDealed] = useState<boolean>(true);
   const [hostCard, setHostCard] = useState<Card>();
   const [botCard, setBotCard] = useState<Card>();
 
   const { deckId, cards } = useSelector((state: AppState) => state.card);
 
   useEffect(() => {
-    dispatch(suffleCardsCreator());
-    // eslint-disable-next-line
-  }, []);
-
-  useEffect(() => {
-    console.log('123123', deckId, cards);
-    if (cards) {
-      setHostCard(cards[0]);
-      setBotCard(cards[1]);
-    }
-  }, [deckId, cards]);
+    setHostCard(cards[0]);
+    setBotCard(cards[1]);
+  }, [cards]);
 
   const handleDeal = () => {
-    dispatch(dealCardsCreator(deckId));
+    if (isDealed) {
+      dispatch(suffleCardsCreator());
+    } else {
+      dispatch(dealCardsCreator(deckId));
+    }
+    setIsDealed(!isDealed);
   };
 
   return (
-    <div className="">
-      <CardView width={100} imageSource={hostCard?.image || CardBackImage} />
-      <CardView width={100} imageSource={botCard?.image || CardBackImage} />
-      <button onClick={handleDeal}>Deal</button>
+    <div className="preview">
+      <div className="card-group">
+        <CardView width={200} imageSource={hostCard?.image || CardBackImage} />
+        <CardView width={200} imageSource={botCard?.image || CardBackImage} />
+      </div>
+      <button className="deal-button" onClick={handleDeal}>
+        {isDealed ? 'Shuffle' : 'Deal'}
+      </button>
     </div>
   );
 };
