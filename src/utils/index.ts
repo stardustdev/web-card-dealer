0967a69dc74e6432b818214264b4a5bf11be2c7a
Stardust <stardustdev@outlook.com>
2020-10-08 16:00:51 -0400

    Update UI

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ export const compareCardValues = (first: string, second: string): number => {
   if (CARD_VALUES.indexOf(first) > CARD_VALUES.indexOf(second)) {
     return 1;
   } else if (CARD_VALUES.indexOf(first) < CARD_VALUES.indexOf(second)) {
-    return 0;
+    return -1;
   }
   return 0;
 };
--- src/view/preview/index.tsx
+++ src/view/preview/index.tsx
@@ -5,8 +5,13 @@ import Player from 'src/containers/Player';
 import Card from 'src/models/Card';
 import { AppState } from 'src/redux/reducer';
 import { suffleCardsCreator, dealCardsCreator } from 'src/redux/card/actions';
+import { compareCardValues } from 'src/utils';
 import UserAvatar from 'src/assets/images/avatar.jpg';
 import BotAvatar from 'src/assets/images/bot.png';
+import WinImage from 'src/assets/images/win.jpg';
+import LoseImage from 'src/assets/images/lose.jpg';
+import DrawImage from 'src/assets/images/draw.png';
+import TransParentImage from 'src/assets/images/transparent.png';
 import './styles.css';
 
 const initialCard = {
@@ -21,12 +26,29 @@ const Preview: React.FC<{}> = (): JSX.Element => {
   const [isDealed, setIsDealed] = useState<boolean>(true);
   const [hostCard, setHostCard] = useState<Card>(initialCard);
   const [botCard, setBotCard] = useState<Card>(initialCard);
+  const [resultImage, setResultImage] = useState<string>(TransParentImage);
 
   const { deckId, cards } = useSelector((state: AppState) => state.card);
 
   useEffect(() => {
     setHostCard(cards[0]);
     setBotCard(cards[1]);
+
+    if (cards.length) {
+      switch (compareCardValues(cards[0].value, cards[1].value)) {
+        case 1:
+          setResultImage(WinImage);
+          return;
+        case -1:
+          setResultImage(LoseImage);
+          return;
+        default:
+          setResultImage(DrawImage);
+          return;
+      }
+    } else {
+      setResultImage(TransParentImage);
+    }
   }, [cards]);
 
   const handleDeal = () => {
@@ -47,6 +69,7 @@ const Preview: React.FC<{}> = (): JSX.Element => {
           cardInfo={hostCard}
           cardWidth={200}
         />
+        <img className="result" src={resultImage} alt="DRAW" />
         <Player
           user={{ name: 'You', avatar: BotAvatar }}
           avatarWidth={100}
